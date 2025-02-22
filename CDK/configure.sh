3651974e21ca72dcb029dfb73d5deb73c962c8c1
Matt Smith <archembaud@hotmail.com>
2025-02-23 09:59:03 +1000

    Ensure Arm compiler is installed by default
    This change adds to the configure.sh script to see the arm compiler
    installed when the instances are deployed.

--- CDK/configure.sh
+++ CDK/configure.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+# Prepare yum update
+sudo yum update
 # Use this to install software packages
 yum groupinstall 'Development Tools' -y
-# Install tmux for long running things
-yum install tmux -y
-# Now try installing the version of pip compatible with 3.8
+sudo yum -y install environment-modules python3 glibc-devel tmux
+sudo yum -y install 'dnf-command(config-manager)' procps psmisc make environment-modules
+sudo yum config-manager --add-repo https://developer.arm.com/packages/ACfL:AmazonLinux-2023/latest/ACfL:AmazonLinux-2023.repo
+sudo yum -y install acfl
+# Now try installing the version of pip compatible with installed python
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 # Install
 python3 get-pip.py
-# Install tmux
-yum install tmux -y
 pip3 install boto3
+# Set modules up for all users
+echo ". /usr/share/Modules/init/bash" >> /etc/bashrc
+echo "module use /opt/arm/modulefiles" >> /etc/bashrc
