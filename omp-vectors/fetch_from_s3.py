f42a4431c4414c9e22b716fe783554ec74102136
Matt Smith <archembaud@hotmail.com>
2025-03-15 10:16:59 +0800

    SVE + OpenMP addition
    This change includes a new series of codes for combination of Open
    MP with SVE vectorization.

---
+++ omp-vectors/fetch_from_s3.py
@@ -0,0 +1,45 @@
+import boto3
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+import matplotlib
+from matplotlib import cm
+
+# If you are using WSL to run this code, you need this line. Otherwise, comment it out.
+matplotlib.use('TkAgg')
+
+# Set the date stamp
+DATE_STAMP = "15-03-25"
+DOWNLOAD_RESULTS = True
+
+if DOWNLOAD_RESULTS:
+    # Open an S3 client and move the file using the date as a prefix
+    s3 = boto3.resource('s3')
+    print("Downloading result file from S3..")
+    s3.meta.client.download_file('sve-results', f'{DATE_STAMP}/results.dat', './results.dat')
+
+# Open the results file and graph some results using matplotlib
+NX = 1024
+NY = 1024
+data = np.genfromtxt('./results.dat')
+x = data[:,0]
+y = data[:,1]
+rho = data[:,2]
+ux = data[:,3]
+uy = data[:,4]
+T = data[:,5]
+mass = data[:,6]
+momx = data[:,7]
+momy = data[:,8]
+eng = data[:,9]
+x_grid = x.reshape((NX, NY))
+y_grid = y.reshape((NX, NY))
+rho_grid = rho.reshape((NX, NY))
+ux_grid = ux.reshape((NX, NY))
+uy_grid = uy.reshape((NX, NY))
+T_grid = T.reshape((NX, NY))
+mass_grid = mass.reshape((NX, NY))
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+# Plot the surface.
+surf = ax.plot_surface(x_grid, y_grid, mass_grid, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+plt.show()
